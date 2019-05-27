6d0fa6cc0fa09e102ee210d7ba94b79cbaf8b834
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-27 22:58:41 +0100

    Create a ListContacts component that manages list of contacts added

---
+++ src/ListContacts.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+const ListContacts = props => {
+	console.log('Props', props);
+	return (
+		<ol className="contact-list">
+			{props.contacts.map(contact => (
+				<li key={contact.id} className="contact-list-item">
+					<div
+						className="contact-avatar"
+						style={{
+							backgroundImage: `url(${contact.avatarURL})`
+						}}
+					/>
+					<div className="contact-details">
+						<p>{contact.name}</p>
+						<p>{contact.email}</p>
+					</div>
+					<button onClick={() => props.onDeleteContact(contact)} className="contact-remove">
+						Remove
+					</button>
+				</li>
+			))}
+		</ol>
+	);
+};
+
+ListContacts.propTypes = {
+	contacts: PropTypes.array.isRequired,
+	onDeleteContact: PropTypes.func.isRequired
+};
+export default ListContacts;
