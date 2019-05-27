f0b895ea511fcf154dea9363081fe0e38017081f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-27 22:53:36 +0100

    Initialise the contacts state and created contacts props that houses all the intialisesd contacts and the onDeleteContact props that houses filters or deletes selected contacts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import ListContacts from '../src/ListContacts';
+import Avatar1 from './img/avatar-1606916_640.png';
+import Avatar2 from './img/avatar-1789663_640.png';
+import Avatar3 from './img/avatar-2027366_640.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		contacts: [
+			{
+				id: 'ryan',
+				name: 'Ryan Florence',
+				email: 'ryan@reacttraining.com',
+				avatarURL: Avatar1
+			},
+
+			{
+				id: 'michael',
+				name: 'Michael Jackson',
+				email: 'michael@reacttraining.com',
+				avatarURL: Avatar2
+			},
+
+			{
+				id: 'tyler',
+				name: 'Tyler McGinnis',
+				email: 'tyler@reacttraining.com',
+				avatarURL: `${Avatar3}`
+			}
+		]
+	};
+	removeContact = contact => {
+		this.setState(state => ({
+			contacts: state.contacts.filter(c => c.id !== contact.id)
+		}));
+	};
+
+	render() {
+		return (
+			<div>
+				<ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact} />
+			</div>
+		);
+	}
 }
 
 export default App;
