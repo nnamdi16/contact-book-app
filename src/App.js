3d22f5fb94f3b5ee6b6774cf5a4776900ebd2312
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-28 10:01:04 +0100

    Add other dependecies such as form-serialise, react-router-dom and escape-string-regexp

--- src/App.js
+++ src/App.js
@@ -2,45 +2,80 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import ListContacts from '../src/ListContacts';
-import Avatar1 from './img/avatar-1606916_640.png';
-import Avatar2 from './img/avatar-1789663_640.png';
-import Avatar3 from './img/avatar-2027366_640.png';
+import { Route } from 'react-router-dom';
+import CreateContact from './CreateContact';
+// import Avatar1 from './img/avatar-1606916_640.png';
+// import Avatar2 from './img/avatar-1789663_640.png';
+// import Avatar3 from './img/avatar-2027366_640.png';
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
 	state = {
-		contacts: [
-			{
-				id: 'ryan',
-				name: 'Ryan Florence',
-				email: 'ryan@reacttraining.com',
-				avatarURL: Avatar1
-			},
+		contacts: []
 
-			{
-				id: 'michael',
-				name: 'Michael Jackson',
-				email: 'michael@reacttraining.com',
-				avatarURL: Avatar2
-			},
+		// contacts: [
+		// 	{
+		// 		id: 'ryan',
+		// 		name: 'Ryan Florence',
+		// 		email: 'ryan@reacttraining.com',
+		// 		avatarURL: Avatar1
+		// 	},
 
-			{
-				id: 'tyler',
-				name: 'Tyler McGinnis',
-				email: 'tyler@reacttraining.com',
-				avatarURL: `${Avatar3}`
-			}
-		]
+		// 	{
+		// 		id: 'michael',
+		// 		name: 'Michael Jackson',
+		// 		email: 'michael@reacttraining.com',
+		// 		avatarURL: Avatar2
+		// 	},
+
+		// 	{
+		// 		id: 'tyler',
+		// 		name: 'Tyler McGinnis',
+		// 		email: 'tyler@reacttraining.com',
+		// 		avatarURL: `${Avatar3}`
+		// 	}
+		// ]
 	};
+	componentDidMount() {
+		ContactsAPI.getAll().then(contacts => {
+			this.setState({ contacts });
+		});
+	}
 	removeContact = contact => {
 		this.setState(state => ({
 			contacts: state.contacts.filter(c => c.id !== contact.id)
 		}));
+		ContactsAPI.remove(contact);
 	};
+	createContact(contact) {
+		ContactsAPI.create(contact).then(contact => {
+			this.setState(state => ({
+				// contacts: [...state.contacts, ...[contact]]
+				contacts: state.contacts.concat([contact])
+			}));
+		});
+	}
 
 	render() {
 		return (
 			<div>
-				<ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact} />
+				<Route
+					exact
+					path="/"
+					render={() => <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact} />}
+				/>
+				<Route
+					exact
+					path="/create"
+					render={({ history }) => (
+						<CreateContact
+							onCreateContact={contact => {
+								this.createContact(contact);
+								history.push('/');
+							}}
+						/>
+					)}
+				/>
 			</div>
 		);
 	}
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,34 +1,87 @@
 import React, { Component } from 'react';
-
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
+import { Link } from 'react-router-dom';
+class ListContacts extends Component {
+	static propTypes = {
+		contacts: PropTypes.array.isRequired,
+		onDeleteContact: PropTypes.func.isRequired
+	};
+	state = {
+		query: ''
+	};
 
-const ListContacts = props => {
-	console.log('Props', props);
-	return (
-		<ol className="contact-list">
-			{props.contacts.map(contact => (
-				<li key={contact.id} className="contact-list-item">
-					<div
-						className="contact-avatar"
-						style={{
-							backgroundImage: `url(${contact.avatarURL})`
-						}}
+	updateQuery = query => {
+		this.setState({
+			query: query.trim()
+		});
+	};
+	clearQuery = () => {
+		// this.setState({
+		// 	query: ''
+		// });
+		this.updateQuery('');
+	};
+	render() {
+		const { query } = this.state;
+		const { contacts, onDeleteContact, onNavigate } = this.props;
+		let showingContacts;
+		if (this.state.query) {
+			//If there are special characters ignore them with the case.
+			const match = new RegExp(escapeRegExp(query), 'i');
+			showingContacts = contacts.filter(contact => match.test(contact.name));
+		} else {
+			showingContacts = contacts;
+		}
+		showingContacts.sort(sortBy('name'));
+		console.log('Props', this.props);
+		return (
+			<div className="list-contacts">
+				{/* {JSON.stringify(this.state)} */}
+				<div className="list-contacts-top">
+					<input
+						className="search-contacts"
+						type="text"
+						placeholder="Search-contacts"
+						value={query}
+						onChange={event => this.updateQuery(event.target.value)}
 					/>
-					<div className="contact-details">
-						<p>{contact.name}</p>
-						<p>{contact.email}</p>
-					</div>
-					<button onClick={() => props.onDeleteContact(contact)} className="contact-remove">
-						Remove
-					</button>
-				</li>
-			))}
-		</ol>
-	);
-};
+					<Link to="/create" onClick={onNavigate} className="add-contact">
+						Add Contact
+					</Link>
+				</div>
 
-ListContacts.propTypes = {
-	contacts: PropTypes.array.isRequired,
-	onDeleteContact: PropTypes.func.isRequired
-};
+				{showingContacts.length !== contacts.length && (
+					<div className="showing-contacts">
+						<span>
+							Now showing {showingContacts.length} of {contacts.length} total{' '}
+						</span>
+						<button onClick={this.clearQuery}>Show All </button>
+					</div>
+				)}
+				<ol className="contact-list">
+					{showingContacts.map(contact => (
+						<li key={contact.id} className="contact-list-item">
+							<div
+								className="contact-avatar"
+								style={{
+									backgroundImage: `url(${contact.avatarURL})`
+								}}
+							/>
+							<div className="contact-details">
+								<p>{contact.name}</p>
+								<p>{contact.email}</p>
+							</div>
+							<button onClick={() => onDeleteContact(contact)} className="contact-remove">
+								Remove
+							</button>
+						</li>
+					))}
+				</ol>
+			</div>
+		);
+	}
+}
+
 export default ListContacts;
